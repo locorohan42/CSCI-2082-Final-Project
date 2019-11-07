6e2eb17bbb4fa3be67d75ee7e0802aac35ca548c
ZachBrier <zachbrier1@gmail.com>
2019-11-07 10:01:03 -0600

    Modified Video.java to replicate Photo.java

--- src/regular/classes/Video.java
+++ src/regular/classes/Video.java
@@ -1,5 +1,56 @@
 package regular.classes;
 
-public class Video {
+import java.util.Date;
 
+public class Video {
+	private int videoNumber;
+	private Date datePosted;
+	private String videoPath;
+	private int videoIncrement;
+	
+	public Video(int videoNumber, String videoPath) {
+		this.videoNumber = 1 + videoIncrement;
+		this.videoPath = videoPath;
+		this.datePosted = getDatePosted();
+		videoIncrement++;
+	}
+
+	public int getVideoNumber() {
+		return videoNumber;
+	}
+
+	public void setVideoNumber(int videoNumber) {
+		this.videoNumber = videoNumber;
+	}
+
+	public Date getDatePosted() {
+		Date datePosted = new Date();
+		return datePosted;
+	}
+
+	public void setDatePosted(Date datePosted) {
+		this.datePosted = datePosted;
+	}
+
+	public String getVideoPath() {
+		return videoPath;
+	}
+
+	public void setVideoPath(String videoPath) {
+		this.videoPath = videoPath;
+	}
+
+	public int getVideoIncrement() {
+		return videoIncrement;
+	}
+
+	public void setVideoIncrement(int videoIncrement) {
+		this.videoIncrement = videoIncrement;
+	}
+	
+	public String toString() {
+		String info = "[Video Number]: " + getVideoNumber() + "[Date Posted]: " 
+	+ (getDatePosted().getMonth()+1) +"/"+ getDatePosted().getDate()+"/" +getDatePosted().getYear();
+		return info;
+	}
 }
