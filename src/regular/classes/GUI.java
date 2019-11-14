49fbc9a785c8e0139434027d59431fb0fab8ccfc
ZachBrier <zachbrier1@gmail.com>
2019-11-14 12:52:28 -0600

    Added basic framework for the GUI

--- src/regular/classes/GUI.java
+++ src/regular/classes/GUI.java
@@ -1,13 +1,52 @@
 package regular.classes;
 
-public class GUI {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+
+public class GUI extends JFrame{
 	
+	// Initialize GUI Components
 	
-	public static void main(String[] args) {
+	// Panels
+	
+	// Labels
+	
+	// Buttons
+	
+	// Text Area(s)
+	
+	// Text Fields
+	
+	// New Class instances
+	
+	public GUI(String title) {
+		super(title);
 		
-	}
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		setBounds(600, 400, 1000, 600); // (x, y, width, height)
+//		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+//		setContentPane(contentPane);
+//		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		
 }
-/*
- * Banana
- */
+	
+	private class BtnListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String callingBtn = e.getActionCommand();
+			
+			if(callingBtn.equals("Login"));
+				
+			}
+			
+		}
+	}
+	
 
