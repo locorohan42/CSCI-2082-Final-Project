72e819b158f1cf6abe4fa103935acce4f36d1479
ZachBrier <zachbrier1@gmail.com>
2019-12-04 20:37:21 -0600

    Changed layout of the login gui | began adding loginInfo.txt functions

--- src/regular/classes/GUI.java
+++ src/regular/classes/GUI.java
@@ -1,16 +1,21 @@
 package regular.classes;
 
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Date;
+import java.util.Scanner;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
-public class GUI {
+public class GUI extends JFrame{
 
 	private JFrame loginWindow = new JFrame();
 
@@ -34,90 +39,89 @@ public class GUI {
 	// New Class instances
 
 	public GUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		loginWindow.setResizable(false);
 		loginWindow.setTitle("Login");
-		loginWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		loginWindow.setBounds(100, 100, 450, 300);
+		loginWindow.setBounds(100, 100, 450, 400);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		loginWindow.setContentPane(contentPane);
+		contentPane.setLayout(new CardLayout(0, 0));
 
-		JPanel loginPanel = new JPanel();
-		contentPane.add(loginPanel, BorderLayout.CENTER);
-		loginPanel.setLayout(null);
+		// Login Pane Components
+		JPanel loginPane = new JPanel();
+		contentPane.add(loginPane, "login");
+		loginPane.setLayout(null);
 
 		JLabel lblUsername = new JLabel("Username");
 		lblUsername.setBounds(80, 70, 80, 20);
-		loginPanel.add(lblUsername);
+		loginPane.add(lblUsername);
 
 		JLabel lblPassword = new JLabel("Password");
 		lblPassword.setBounds(80, 110, 80, 20);
-		loginPanel.add(lblPassword);
+		loginPane.add(lblPassword);
 
 		loginField = new JTextField();
 		loginField.setBounds(164, 70, 100, 20);
-		loginPanel.add(loginField);
+		loginPane.add(loginField);
 		loginField.setColumns(10);
 
 		passwordField = new JPasswordField();
-		passwordField.setBounds(163, 110, 100, 20);
-		loginPanel.add(passwordField);
-
-		JButton btnRegister = new JButton("Register");
-		btnRegister.setBounds(80, 170, 90, 24);
-		loginPanel.add(btnRegister);
-		btnRegister.addActionListener(new BtnListener());
+		passwordField.setBounds(164, 110, 100, 20);
+		loginPane.add(passwordField);
 
 		JButton btnLogin = new JButton("Login");
 		btnLogin.setBounds(175, 170, 90, 24);
-		loginPanel.add(btnLogin);
+		loginPane.add(btnLogin);
 		btnLogin.addActionListener(new BtnListener());
 
 		JButton btnCancel = new JButton("Cancel");
 		btnCancel.setBounds(270, 170, 90, 24);
-		loginPanel.add(btnCancel);
+		loginPane.add(btnCancel);
 		btnCancel.addActionListener(new BtnListener());
 
-		//
-		JPanel registerPanel = new JPanel();
+		// Register Pane Components
+		JPanel registerPane = new JPanel();
 		registerPane = new JPanel();
-		registerPane.add(registerPanel, BorderLayout.CENTER);
-		registerPanel.setLayout(null);
+		contentPane.add(registerPane, "register");
+		registerPane.setLayout(null);
 
-		JLabel lblCreateUsername = new JLabel("Enter Username");
-		lblCreateUsername.setBounds(80, 70, 90, 20);
-		registerPanel.add(lblCreateUsername);
+		JLabel lblEnterUsername = new JLabel("Enter Username");
+		lblEnterUsername.setBounds(80, 70, 100, 20);
+		registerPane.add(lblEnterUsername);
 
-		JLabel lblEmail = new JLabel("Enter Email");
-		lblEmail.setBounds(80, 110, 90, 20);
-		registerPanel.add(lblEmail);
+		JLabel lblEnterEmail = new JLabel("Enter Email");
+		lblEnterEmail.setBounds(80, 110, 100, 20);
+		registerPane.add(lblEnterEmail);
 
-		JTextField userNameTxtField = new JTextField();
-		userNameTxtField.setBounds(180, 70, 100, 20);
-		registerPanel.add(userNameTxtField);
-		userNameTxtField.setColumns(10);
+		JTextField enterUserTxtField = new JTextField();
+		enterUserTxtField.setBounds(190, 70, 100, 20);
+		registerPane.add(enterUserTxtField);
+		enterUserTxtField.setColumns(10);
 
-		JTextField emailTxtField = new JTextField();
-		emailTxtField.setBounds(180, 110, 100, 20);
-		registerPanel.add(emailTxtField);
-		emailTxtField.setColumns(10);
+		JTextField enterEmailTxtField = new JTextField();
+		enterEmailTxtField.setBounds(190, 110, 100, 20);
+		registerPane.add(enterEmailTxtField);
+		enterEmailTxtField.setColumns(10);
 
-		JLabel lblEnterPassword = new JLabel("Create Password");
-		lblEnterPassword.setBounds(80, 150, 90, 20);
-		registerPanel.add(lblEnterPassword);
+		JLabel lblCreatePassword = new JLabel("Create Password");
+		lblCreatePassword.setBounds(80, 150, 100, 20);
+		registerPane.add(lblCreatePassword);
 
-		JButton backButton = new JButton("<- Back to Login");
-		backButton.setBounds(80, 206, 110, 23);
-		registerPanel.add(backButton);
-
-		JButton regButton = new JButton("Register");
-		regButton.setBounds(200, 206, 89, 23);
-		registerPanel.add(regButton);
+		JButton btnCreateUser = new JButton("Create User");
+		btnCreateUser.setBounds(200, 206, 110, 24);
+		registerPane.add(btnCreateUser);
 
 		JPasswordField createPasswordField = new JPasswordField();
-		createPasswordField.setBounds(180, 150, 100, 20);
-		registerPanel.add(createPasswordField);
+		createPasswordField.setBounds(190, 150, 100, 20);
+		registerPane.add(createPasswordField);
+		
+		// Tabs 
+		JTabbedPane tabbedPane = new JTabbedPane();
+		
+		contentPane.add(tabbedPane, BorderLayout.CENTER);
+		tabbedPane.addTab("Login", loginPane);
+		tabbedPane.addTab("Register", registerPane);
 
 		JFrame mainWindow = new JFrame();
 		mainWindow.setBounds(100, 100, 450, 300);
@@ -146,26 +150,50 @@ public class GUI {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			String callingBtn = e.getActionCommand();
+			CardLayout cardlayout = new CardLayout();
+			
 
 			if (callingBtn.equals("Login")) {
+				
+				try {
+					File file = new File("\\loginInfo.txt");
+					Scanner scan = new Scanner(file);
+					String line = null;
+					FileWriter fileWrite = new FileWriter(file, true);
+					
+					String userTxt = " ";
+					String passTxt = " ";
+					
+					while (scan.hasNext()) {
+						userTxt = scan.nextLine();
+						passTxt = scan.nextLine();
+						
+						if (username.equals(userTxt) && password.equals(passTxt)) {
+							JOptionPane.showMessageDialog(contentPane, "Login Successful!");
+							mainWindow window = new mainWindow();
+							window.setVisible(true);
+							loginWindow.setVisible(false);
+							loginWindow.dispose();
+
+						} else if(username.equals("") && password.equals("")){
+							JOptionPane.showMessageDialog(contentPane, "Please enter Username and Passwors");
+						}
+						
+						else {
+							JOptionPane.showMessageDialog(contentPane, "Username or password is incorrect");
+						}
+						
+					}
+				} catch (IOException d){
+					d.printStackTrace();
+				}
+				
 				username = loginField.getText();
 				password = new String(passwordField.getPassword());
 
-				if (username.equals("user") && password.equals("pass")) {
-					JOptionPane.showMessageDialog(contentPane, "Login Successful!");
-					mainWindow window = new mainWindow();
-					window.setVisible(true);
-					loginWindow.setVisible(false);
-					loginWindow.dispose();
-
-				} else {
-					JOptionPane.showMessageDialog(contentPane, "Username or password is incorrect");
-				}
 			}
 
-			else if (callingBtn.equals("Register")) {
-//				contentPane.add(registerPane);
-//				contentPane.remove(contentPane);
+			else if (callingBtn.equals("Create User")) {
 
 //				email = new String(emailField.getText());
 				User createUser = new User(username, password, email);
@@ -179,6 +207,10 @@ public class GUI {
 
 			else if (callingBtn.equals("Cancel")) {
 				System.exit(0);
+			}
+			
+			else if(callingBtn.equals("<- Back")) {
+				cardlayout.next(registerPane);
 			}
 
 		}
